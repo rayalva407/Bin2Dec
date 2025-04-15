39c4549b4a6f36ab296a4e0ecaf5d9e438e01fd1
Raymundo Alva <ralv156@wgu.edu>
2025-04-15 08:38:16 -0600

    Displays the correct binary number

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,34 @@
-import {Text, StyleSheet, TextInput, View, Button,} from "react-native";
+import {Text, StyleSheet, TextInput, View, Button} from "react-native";
 import {useState} from "react";
 
 export default function Index() {
   const [inputValue, setInputValue] = useState("");
-  const binToDec = (binary :string): number => {
-    return parseInt(binary, 10);
+  const [decimal, setDecimal] = useState("");
+
+  const binToDec = (binary :string): string => {
+      for (let i = 0; i <= binary.length - 1; i++) {
+          if (binary.charAt(i) !== "0" && binary.charAt(i) !== "1") {
+              return "Binary should only contain 1s or 0s. Try again."
+          }
+
+      }
+
+      return String(parseInt(binary, 2))
   };
 
   return (
       <View style={styles.view}>
         <Text style={styles.headerText}>Bin2Dec</Text>
-        <View><Text style={styles.decimal}>0</Text></View>
+        <View><Text style={styles.decimal}>{decimal}</Text></View>
         <TextInput
             style={styles.input}
             maxLength={8}
+            onChangeText={setInputValue}
             placeholder="Enter a binary number up to 8 digits"
         />
         <Button
             title={"Submit"}
-            onPress={binToDec("10")}
+            onPress={() => setDecimal(binToDec(inputValue))}
         />
       </View>
   );
